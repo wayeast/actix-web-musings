cf5cfd546120f681f0f1357260b425b254678a8a
Huston Bokinsky <huston@deepgram.com>
2018-08-25 16:21:59 -0700

    state is now shared between app threads

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,24 @@
 extern crate actix_web;
 
 use actix_web::{http, server, App, HttpRequest};
-use std::cell::Cell;
+use std::sync::{Arc, Mutex};
 
-// NB: testing this with curl requests may look funny.  On my machine,
-//  actix-web starts 8 server threads.  Since each thread has its own
-//  state, I had to submit 8 curl requests before seeing count increment.
 struct State {
-    counter: Cell<usize>,
+    counter: Arc<Mutex<usize>>,
 }
 
 fn index(req: &HttpRequest<State>) -> String {
-    let current_count = req.state().counter.get();
-    let updated_count = current_count + 1;
-    req.state().counter.set(updated_count);
+    let mut count = req.state().counter.lock().unwrap();
+    *count += 1;
 
-    format!("Current count: {}", updated_count)
+    format!("Current count: {}", count)
 }
 
 fn main() {
-    server::new(|| {
-        App::with_state(State { counter: Cell::new(0) })
+    let count = Arc::new(Mutex::new(0_usize));
+
+    server::new(move || {
+        App::with_state(State { counter: Arc::clone(&count) })
             .resource("/", |r| r.method(http::Method::GET).f(index))
     })
         .bind("127.0.0.1:8088")
