e2687f0e633b74f4513ee4350802344d3a7013cc
Huston Bokinsky <huston@deepgram.com>
2018-08-25 15:53:16 -0700

    one shared state per server thread

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
 extern crate actix_web;
-use actix_web::{server, App, HttpResponse};
+
+use actix_web::{http, server, App, HttpRequest};
+use std::cell::Cell;
+
+// NB: testing this with curl requests may look funny.  On my machine,
+//  actix-web starts 8 server threads.  Since each thread has its own
+//  state, I had to submit 8 curl requests before seeing count increment.
+struct State {
+    counter: Cell<usize>,
+}
+
+fn index(req: &HttpRequest<State>) -> String {
+    let current_count = req.state().counter.get();
+    let updated_count = current_count + 1;
+    req.state().counter.set(updated_count);
+
+    format!("Current count: {}", updated_count)
+}
 
 fn main() {
-    let svr = server::new(|| {
-        vec![
-            App::new()
-                .prefix("/app1")
-                .resource("/", |r| r.f(|_| HttpResponse::Ok())),
-            App::new()
-                .prefix("/app2")
-                .resource("/", |r| r.f(|_| HttpResponse::Ok())),
-            App::new().resource("/", |r| r.f(|_| HttpResponse::Ok())),
-        ]
-    });
-
-    svr.bind("127.0.0.1:8088")
+    server::new(|| {
+        App::with_state(State { counter: Cell::new(0) })
+            .resource("/", |r| r.method(http::Method::GET).f(index))
+    })
+        .bind("127.0.0.1:8088")
         .unwrap()
-        .run();
+        .run()
 }
