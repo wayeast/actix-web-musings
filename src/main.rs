55a9b3aa94b6fba72b66ae0c2df2163566d6d454
Huston Bokinsky <huston@deepgram.com>
2018-08-27 10:25:55 -0700

    diff prefix means diff path

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,20 @@
 extern crate actix_web;
 
-use actix_web::{server, App, HttpResponse};
+use actix_web::{server, App, HttpRequest};
 
-// NB: this example, copied directly from the actix-web getting started
-//  guide, is intended to show how all App instances in a server need to
-//  be of the same type (eg. App<State1>).  Try removing the `.boxed()`
-//  directives to see what happens.  Also, try removing one and leaving
-//  the other.
-struct State1;
-struct State2;
+fn index(req: &HttpRequest) -> String {
+    format!("path: '{}'\n", req.path())
+}
 
 fn main() {
     server::new(|| {
         vec![
-            App::with_state(State1)
-                .prefix("/app1")
-                .resource("/", |r| r.f(|_| HttpResponse::Ok()))
-                .boxed(),
-            App::with_state(State2)
-                .prefix("/app2")
-                .resource("/", |r| r.f(|_| HttpResponse::Ok()))
-                .boxed(),
-                ]
+            App::new()
+                .prefix("/prefixed")
+                .resource("/", |r| r.f(index)),
+            App::new()
+                .resource("/", |r| r.f(index)),
+        ]
     }).bind("127.0.0.1:8088")
         .unwrap()
         .run()
