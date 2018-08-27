16789615ad1199c0b820ba0d7fb7804a3d8dbc56
Huston Bokinsky <huston@deepgram.com>
2018-08-27 10:40:03 -0700

    Use predicate to filter incoming requests

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,16 @@
 extern crate actix_web;
 
-use actix_web::{server, App, HttpRequest};
+use actix_web::{pred, server, App, HttpRequest};
 
 fn index(req: &HttpRequest) -> String {
-    format!("path: '{}'\n", req.path())
+    format!("headers: {:?}\n", req.headers())
 }
 
 fn main() {
-    server::new(|| {
-        vec![
-            App::new()
-                .prefix("/prefixed")
+    server::new(|| App::new()
+                .filter(pred::Header("Content-Type", "kra-kra"))
                 .resource("/", |r| r.f(index)),
-            App::new()
-                .resource("/", |r| r.f(index)),
-        ]
-    }).bind("127.0.0.1:8088")
+    ).bind("127.0.0.1:8088")
         .unwrap()
         .run()
 }
