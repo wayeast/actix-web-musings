86f1ebd4053ba82e516db0e649bc341b08c88323
Huston Bokinsky <huston@deepgram.com>
2018-08-28 18:51:59 -0700

    basic async handlers

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
 extern crate actix_web;
+extern crate futures;
 
-use actix_web::{pred, server, App, HttpRequest};
+use actix_web::*;
+use futures::future::{Future, result};
 
-fn index(req: &HttpRequest) -> String {
-    format!("headers: {:?}\n", req.headers())
+fn index(_: &HttpRequest) -> Box<Future<Item=HttpResponse, Error=Error>> {
+
+    result(Ok(HttpResponse::Ok()
+              .content_type("text/html")
+              .body(format!("Hello!"))))
+           .responder()
+}
+
+fn index2(_: &HttpRequest) -> Box<Future<Item=&'static str, Error=Error>> {
+    result(Ok("Welcome!"))
+        .responder()
 }
 
 fn main() {
-    server::new(|| App::new()
-                .filter(pred::Header("Content-Type", "kra-kra"))
-                .resource("/", |r| r.f(index)),
-    ).bind("127.0.0.1:8088")
+    server::new(|| {
+        App::new()
+            .resource("/async1", |r| r.route().a(index))
+            .resource("/async2", |r| r.route().a(index2))
+    })
+        .bind("127.0.0.1:8088")
         .unwrap()
-        .run()
+        .run();
 }
